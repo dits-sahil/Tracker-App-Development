10f11fc722d7b600c930aca8624e109dc9b2bd68
karamdeepwins <karamdeepkaurwins@gmail.com>
2024-03-05 13:35:01 +0530

    update Dev

--- src/app/feature/manager/manager.module.ts
+++ src/app/feature/manager/manager.module.ts
@@ -2,15 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ManagerComponent } from './manager.component';
 import { SharedModule } from "../../shared/shared.module";
-import { AppRoutingModule } from 'src/app/app-routing.module';
-import { AssignmentListComponent } from './components/assignment-list/assignment-list.component';
 import { ManagerRoutingModule } from './manager-routing.module';
 import { LayoutModule } from 'src/app/shared/components/layout/layout.module';
+import { UsersComponent } from './components/users/users.component';
 
 @NgModule({
     declarations: [
         ManagerComponent,
-        AssignmentListComponent
+        UsersComponent,
     ],
     imports: [
         CommonModule,
