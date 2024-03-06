eb584cb52b5fd71214f54cbed667dc6f633963b9
karamdeepwins <karamdeepkaurwins@gmail.com>
2024-03-06 11:25:05 +0530

    update UI

--- src/app/feature/admin/admin.component.ts
+++ src/app/feature/admin/admin.component.ts
@@ -8,9 +8,17 @@ import { SidenavService } from 'src/app/core/services/sidenav.service';
   styleUrls: ['./admin.component.scss']
 })
 export class AdminComponent {
+  loginDetails:any;
+  isExpand: boolean = true;
   constructor(private router:Router){
     router.navigate(['admin','users'])
   }
   ngOnInit(){
+    this.loginDetails = localStorage.getItem('user');
+    const storedItems = JSON.parse(this.loginDetails.getItem('dataSource'));
+    console.log(this.loginDetails.role)
+  }
+  public expandOffItem(inExpand: boolean) {
+    this.isExpand = inExpand
   }
 }
--- src/app/shared/components/layout/layout.component.ts
+++ src/app/shared/components/layout/layout.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 
 @Component({
   selector: 'app-layout',
@@ -6,11 +6,34 @@ import { Component } from '@angular/core';
   styleUrls: ['./layout.component.scss']
 })
 export class LayoutComponent {
+  @Output() getExpandSidebar = new EventEmitter<boolean>();
+  @Input() inputIsExpand: boolean = true 
+  isExpand = true;
+  
+  ngOnInit(){
+    if (window.innerWidth >= 768 && window.innerWidth < 992 ) {
+      this.isExpand =true
+      } else {
+       this.isExpand=false
+      }
+  }
   toggleSidebar() {
+
     // Handle opening/closing of sidebar
   }
 
   closeSidebar() {
     // Handle closing of sidebar
+  }
+  
+  public expandOffItem(isExpandChild:boolean){
+    // if(isExpandChild){
+    //   $("body").removeClass('showLess')
+    //   $("body").addClass('showMore')
+    // }else{
+    //   $("body").removeClass('showMore')
+    //   $("body").addClass('showLess')
+    // }
+    this.getExpandSidebar.emit(isExpandChild)
   }
 }
