320d875107b1307c4875af4dbb161199726f5673
sahil verma <sahil.verma@ditstek.com>
2024-03-07 15:02:23 +0530

    sie bar fixed

--- src/app/core/guards/role.guard.ts
+++ src/app/core/guards/role.guard.ts
@@ -4,9 +4,9 @@ import { CanActivateFn, Router } from '@angular/router';
 export const roleGuard: CanActivateFn = (route, state) => {
   const router: Router = inject(Router);
   const user = JSON.parse(localStorage.getItem('user')!);
-  if (user && user.role == 1){  return router.navigate(['/admin'])}
-  if (user && user.role == 2){  return router.navigate(['/manager'])}
-  if (user && user.role == 3){  return router.navigate(['/user'])}
+  if (user && user.role == 1){  return router.navigate(['/admin/dashboard'])}
+  if (user && user.role == 2){  return router.navigate(['/manager/dashboard'])}
+  if (user && user.role == 3){  return router.navigate(['/user/dashboard'])}
   router.navigate(['/login'])
   return false;
 };
