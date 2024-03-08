be554ac8c27104a605c28a02b4d5f393fe48a5f7
sahil verma <sahil.verma@ditstek.com>
2024-03-08 10:30:43 +0530

    add backend

---
+++ src/backend/index.js
@@ -0,0 +1,33 @@
+const admin = require('firebase-admin');
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const serviceAccount  = require('./tracker-app-5a80b-firebase-adminsdk-c4g0m-1e229e5e22.json');
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
+
+const app = express();
+app.use(cors()); // Add this line to enable CORS
+app.use(bodyParser.json());
+
+app.post('/getCustomToken', (req, res) => {
+  const uid = req.body.uid; // Get UID from request
+  if (!uid) {
+    res.status(400).send('UID is required');
+    return;
+  }
+
+  admin.auth().createCustomToken(uid)
+    .then((customToken) => {
+      res.json({ customToken });
+    })
+    .catch((error) => {
+      console.error('Error creating custom token:', error);
+      res.status(500).send('Internal server error');
+    });
+});
+
+app.listen(3000, () => {
+  console.log('Server is running on port 3000');
+});
